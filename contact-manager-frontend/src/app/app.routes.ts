3231654109c8cc1316ab4178d5984c4414b8352f
cyptrix12 <120047588+cyptrix12@users.noreply.github.com>
2025-04-20 01:37:03 +0200

    feat: add auth guard
    not logged in users, when trying to open /contacts/add page are redirected to /login

--- contact-manager-frontend/src/app/app.routes.ts
+++ contact-manager-frontend/src/app/app.routes.ts
@@ -1,17 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
-import { ContactsComponent } from './pages/contacts/contacts.component';
 import { ContactsListComponent } from './pages/contacts-list/contacts-list.component';
 import { ContactDetailsComponent } from './pages/contacts-details/contacts-details.component';
 import { ContactAddComponent } from './pages/contact-add/contact-add.component';
+import { AuthGuard } from './guards/auth.guard';
 
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'contacts/add', component: ContactAddComponent },
+  { path: 'contacts/add', component: ContactAddComponent, canActivate: [AuthGuard] },
   { path: 'contacts/:id', component: ContactDetailsComponent },
   { path: 'contacts', component: ContactsListComponent },
   
